9cb36e0554c2b85a68b4eda159c0d5cd8d587bc1
ToquiNovic <toquicaagudelo123@gmail.com>
2026-01-13 15:06:53 -0500

    feat: update page meta for UsersPage with relevant title and description

--- src/pages/Users/usersPage.tsx
+++ src/pages/Users/usersPage.tsx
@@ -9,8 +9,8 @@ export const UsersPage = () => {
   return (
     <>
       <PageMeta
-        title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+        title="Usuarios - Restaurant Admin"
+        description="Lista de usuarios del sistema de restaurante"
       />
       <PageBreadcrumb pageTitle="Basic Tables" />
       <div className="space-y-6">
